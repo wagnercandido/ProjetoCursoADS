f5562c859bfb0067bab225730b673bf2e21055b3
wagnercandido <wagnercssilva@gmail.com>
2018-10-29 18:31:34 -0300

    Alterations at the color of some fonts

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,9 @@
-$('.nav a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
+$(function () {
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop() > 100) {
+			$(".sidebar-wrapper").addClass("cor-Nav");
+		} else {
+			$(".sidebar-wrapper").removeClass("cor-Nav");
+		}
+	});
 });
